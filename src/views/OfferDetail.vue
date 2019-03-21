9432781376c7c9e1477ff0ebe666b0fd50d50117
Alex <work@xingyongtao.xyz>
2019-03-21 16:47:00 +0800

    update address

--- src/views/OfferDetail.vue
+++ src/views/OfferDetail.vue
@@ -26,7 +26,7 @@
 </template>
 <script>
     import abi_ from '@/assets/abi.js'
-    let token_address = '0x51fC15CA47034bDF62F6e0fd0E37AB389832994C'
+    let token_address = '0xA3434f0ABAC66f46ABCf2E6c47488C00aA1980Cb'
 
     export default {
         data() {
