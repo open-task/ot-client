eaabc8cd1ed8b491056b9950951d965bd7c80026
Alex <work@xingyongtao.xyz>
2019-03-27 17:13:30 +0800

    update opentask address to v0.3.1

--- src/assets/test.js
+++ src/assets/test.js
@@ -1,7 +1,7 @@
 import det_abi from '@/assets/detabi.js'
 import abi from '@/assets/abi.js'
 
-let token_address = '0x51fC15CA47034bDF62F6e0fd0E37AB389832994C'
+let token_address = '0xA3434f0ABAC66f46ABCf2E6c47488C00aA1980Cb'
 let det_address = "0x6ffF60A882CE1Cd793dC14261Eec0f0d6A470E21"
 let web3api = new Web3(web3.currentProvider);
 let accounts = web3api.eth.accounts;
